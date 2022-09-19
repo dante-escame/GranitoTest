16655c284fe3f96c0da803a381c9a2ef1c1f77fd
Dante Escame <dante.escame@unesp.br>
2022-09-19 10:52:53 -0300

    Feature: aplicando os arquivos do docker + docker compose para usar multi-containers na aplicação.

--- GranitoTest.TaxApi/Startup.cs
+++ GranitoTest.TaxApi/Startup.cs
@@ -37,12 +37,14 @@ namespace GranitoTest.TaxApi
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      if (env.IsDevelopment())
-      {
+      /*if (env.IsDevelopment())
+      {*/
         app.UseDeveloperExceptionPage();
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GranitoTest.TaxApi v1"));
-      }
+      //}
+
+      //app.UseHttpsRedirection();
 
       app.UseRouting();
 

1d8996a91983055edab1357d903d3315466fa6f4
Dante Escame <dante.escame@unesp.br>
2022-09-19 14:58:15 -0300

    Fix: docker compose agora está funcionando na minha máquina local.

--- GranitoTest.TaxApi/Startup.cs
+++ GranitoTest.TaxApi/Startup.cs
@@ -22,10 +22,22 @@ namespace GranitoTest.TaxApi
     }
 
     public IConfiguration Configuration { get; }
+    readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddCors(options =>
+      {
+        options.AddPolicy(name: MyAllowSpecificOrigins,
+                          policy =>
+                          {
+                            policy.WithOrigins("http://example.com",
+                                                    "http://localhost",
+                                                    "http://127.0.0.1",
+                                                    "http://host.docker.internal");
+                          });
+      });
 
       services.AddControllers();
       services.AddSwaggerGen(c =>
